8119719ed8d8335e245b3b04a68cfd6446922b8b
Naoki Yaguchi <murooka.1129.x50@gmail.com>
2018-06-18 00:35:37 +0900

    Add events

--- contracts/WrappedEther.sol
+++ contracts/WrappedEther.sol
@@ -3,10 +3,15 @@ pragma solidity ^0.4.23;
 import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 
 contract WrappedEther is StandardToken {
+  event Deposited(address indexed owner, uint value);
+  event Withdrawn(address indexed owner, uint value);
+
   function deposit() public payable {
     require(msg.value > 0);
     balances[msg.sender] += msg.value;
     totalSupply_ += msg.value;
+
+    emit Deposited(msg.sender, msg.value);
   }
   function withdraw(uint _value) public {
     require(_value > 0);
@@ -14,5 +19,7 @@ contract WrappedEther is StandardToken {
     balances[msg.sender] -= _value;
     totalSupply_ -= _value;
     msg.sender.transfer(_value);
+
+    emit Withdrawn(msg.sender, _value);
   }
 }
